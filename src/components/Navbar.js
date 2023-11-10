7fea9cfe96b1fabbd7aa18c85b6d6bf03ae907c1
MariellaHeikkila <mariella.heikkila@gmail.com>
2023-11-10 14:35:55 +0200

    koodikokeilut

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    return (
+<nav>
+      <ul>
+        <li><a href="/">Koti</a></li>
+        <li><a href="/tuotteet">Tuotteet</a></li>
+        <li><a href="/yhteystiedot">Yhteystiedot</a></li>
+      </ul>
+    </nav>
+    );
+  }
+  
+  export default Navbar;
